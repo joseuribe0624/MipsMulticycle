edd467e8a5f31658510a8642ea168c72cfed24d0
jozdashh <jozdashh@javerianacali.edu.co>
2018-10-15 09:35:48 -0500

    Digitos en 7 segmentos pasados a Array.
    Así es más fácil acceder a cada dígito usando la instrucción lb (load 
    byte).

--- MipsCode/test.s
+++ MipsCode/test.s
@@ -1,30 +1,23 @@
     .data
-        ## DIGITOS CODIFICADOS EN 7 SEGMENTOS ##
-        ZERO:   .byte   0x7e
-        ONE:    .byte   0x06
-        TWO:    .byte   0x6d
-        TREE:   .byte   0x79
-        FOUR:   .byte   0x33
-        FIVE:   .byte   0x5b
-        SIX:    .byte   0x5f
-        SEVEN:  .byte   0x70
-        EIGHT:  .byte   0x7f
-        NINE:   .byte   0x7b
+# DIGITOS CODIFICADOS EN 7 SEGMENTOS #
+DIGIT:  .byte   0x7e, 0x06, 0x6d, 0x79, 0x33, 0x5b, 0x5f, 0x70, 0x7f, 0x7b
+# Los digitos están en orden (0, 1, 2, 3, ..., 9)
 
-        ## CARACTERES CODIFICADOS EN 7 SEGMENTOS ##
-        # -- Podriamos cambiar todas las letras a lowercase
-        A:  .byte   0x77
-        C:  .byte   0x4e
-        E:  .byte   0x4f
-        G:  .byte   0x5e
-        H:  .byte   0x17 # Lowercase
-        L:  .byte   0x0e
-        N:  .byte   0x76
-        O:  .byte   0x1d # Lowercase
-        P:  .byte   0x67
-        R:  .byte   0x05 # Lowercase
-        T:  .byte   0x0f
-        X:  .byte   0x37 # Uppercase
+# CARACTERES CODIFICADOS EN 7 SEGMENTOS #
+A:  .byte   0x77
+C:  .byte   0x4e
+E:  .byte   0x4f
+G:  .byte   0x5e
+H:  .byte   0x17 # Lowercase
+L:  .byte   0x0e
+N:  .byte   0x76
+O:  .byte   0x1d # Lowercase
+P:  .byte   0x67
+R:  .byte   0x05 # Lowercase
+S:  .byte   0x5b # Igual que el digito '5' (6 + DIGIT)
+T:  .byte   0x0f # Lowercase
+X:  .byte   0x37 # Uppercase (enrealidad es una H)
+
 
     .text
 INICIO:
