01115684818643be054e0e4491d26c9ca6c0714b
jozdashh <jozdashh@javerianacali.edu.co>
2018-10-28 17:48:44 -0500

    Unidad de control arreglada y soportando addi Faltaba un estado extra en la unidad anterior, y agregué uno más para aceptar addi. Todavía falta considerar una o varias instrucciones para cargar constantes mas grandes que 9 bits (pero sin signo) en registros.

--- Multicycle/Control.vhd
+++ Multicycle/Control.vhd
@@ -3,185 +3,262 @@ use ieee.std_logic_1164.all;
 
 entity Control is
 	port(
-		opcode: in std_logic_vector (3 downto 0);
+		opcode: 		in std_logic_vector (3 downto 0);
 		clk, reset: in std_logic;
-		Branch, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, ALUSrcA, RegWrite, RegDst: out std_logic;
-		PCSrc, ALUOp, ALUSrcB: out std_logic_vector (1 downto 0)
+
+		Branch,
+		PCWrite,
+		IorD,
+		MemRead,
+		MemWrite,
+		MemtoReg,
+		IRWrite,
+		ALUSrcA,
+		RegWrite,
+		RegDst: 	out std_logic;
+
+		PCSrc,
+		ALUOp,
+		ALUSrcB: 	out std_logic_vector (1 downto 0)
 	);
 end Control;
 
--- LW: 1010
--- SW: 1011
--- R: 0000
--- BEQ: 0100
--- J: 0010
+-- (OPCODES)
+-- TIPO R	: 0000
+--				(campo function)
+-- 			ADD		: 000
+-- 			SUB		: 001 (por si acaso)
+-- 			AND		: 010 (por si acaso)
+-- 			OR		: 011 (por si acaso)
+--
+-- ADDI 	: 0001
+-- LW 		: 0010
+-- SW 		: 0011
+--
+-- BEQ		: 0100
+-- J			: 0101
 
 architecture behavior of Control is
-	signal state: std_logic_vector (5 downto 0);
-	signal next_state: std_logic_vector (5 downto 0);
+	-- FALTAN POSIBLES NUEVOS ESTADOS PARA LUI & ORI
+	type state_type is (s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11);
+	signal state: state_type;
+	-- Los estados están numerados con respecto al recorrido por niveles (bfs)
+	-- del arbol de estados en la pag 28 del pdf MipsMulticycle.
 	begin
 		process (clk, reset)
 			begin
 				if (reset = '1') then
-					state <= "000000";
+					state <= s1;
 				elsif (clk'event and clk='1') then
-					state <= next_state;
-				end if;
-		end process;
+					case ( state ) is
+						when s1 => -- Fetch
+								state 		<= s2;
+								--
+								IorD 			<= '0';
+								MemRead 	<= '1';
+								ALUSrcA 	<= '0';
+								ALUSrcB 	<= "01";
+								ALUOp			<= "00";
+								PCSrc 		<= "00";
+								IRWrite 	<= '1';
+								PCWrite 	<= '1';
+								--
+								MemWrite 	<= '0';
+								MemtoReg 	<= '0';
+								Branch 		<= '0';
+								RegWrite 	<= '0';
+								RegDst 		<= 'X';
 
-		process (opcode, state)
-			begin
-				if (state = "0000") then
-					IorD <= '0';
-					MemRead <= '1';
-					ALUSrcA <= '0';
-					ALUSrcB <= "01";
-					ALUOp <= "00";
-					PCSrc <= "00";
-					IRWrite <= '1';
-					PCWrite <= '1';
-					next_state <= "000001";
-					--
-					MemWrite <= '0';
-					MemtoReg <= '0';
-					Branch <= '0';
-					RegWrite <= '0';
-					RegDst <= 'X';
-				elsif (state = "000001") then
-					ALUSrcA <= '0';
-					ALUSrcB <= "11";
-					ALUOp <= "00";
-					case opcode is 
-						when "1010"|"1011" => next_state <= "000010";
-						when "0000" => next_state <= "000110";
-						when "0100" => next_state <= "000100";
-						when others => next_state <= "001001";
+						when s2 => -- Decode
+								ALUSrcA 	<= '0';
+								ALUSrcB 	<= "11";
+								ALUOp 		<= "00";
+								--
+								case ( opcode ) is
+									when "0000" => state <= s4; -- tipo R
+									when "0001" | "0010" | "0011" => state <= s3; -- addi | lw | sw
+									when "0100" => state <= s5; -- beq
+									when others => state <= s6; -- jump
+								end case;
+								--
+								PCSrc 		<= "00";
+								IRWrite 	<= '0';
+								PCWrite 	<= '0';
+								MemWrite 	<= '0';
+								MemRead 	<= '0';
+								RegWrite 	<= '0';
+								Branch 		<= '0';
+								RegDst	 	<= 'X';
+								IorD 			<= 'X';
+								MemtoReg 	<= 'X';
+
+						when s3 => -- MemAddr
+								ALUSrcA 	<= '1';
+								ALUSrcB 	<= "10";
+								ALUOp 		<= "00";
+								--
+								case ( opcode ) is
+									when "0010" => state <= s7; -- lw
+									when "0011" => state <= s8; -- sw
+									when others => state <= s9; -- addi
+								end case;
+								--
+								PCSrc 		<= "00";
+								IRWrite 	<= '0';
+								PCWrite 	<= '0';
+								MemWrite 	<= '0';
+								MemRead 	<= '0';
+								RegWrite 	<= '0';
+								Branch 		<= '0';
+								RegDst 		<= 'X';
+								IorD 			<= 'X';
+								MemtoReg 	<= 'X';
+
+						when s4 => -- Execute
+								state			<= s10;
+								--
+								ALUSrcA 	<= '1';
+								ALUSrcB 	<= "00";
+								ALUOp 		<= "10";
+								--
+								PCSrc 		<= "00";
+								IRWrite 	<= '0';
+								PCWrite 	<= '0';
+								MemWrite 	<= '0';
+								MemRead 	<= '0';
+								RegWrite 	<= '0';
+								Branch 		<= '0';
+								RegDst 		<= 'X';
+								IorD 			<= 'X';
+								MemtoReg 	<= 'X';
+
+						when s5 => -- Branch
+								state 		<= s1;
+								--
+								ALUSrcA 	<= '1';
+								ALUSrcB 	<= "00";
+								ALUOp 		<= "01";
+								PCSrc 		<= "01";
+								Branch 		<= '1';
+								--
+								IRWrite 	<= '0';
+								PCWrite		<= '0';
+								MemWrite 	<= '0';
+								MemRead 	<= '0';
+								RegWrite 	<= '0';
+								RegDst 		<= 'X';
+								IorD 			<= 'X';
+								MemtoReg 	<= 'X';
+
+						when s6 => -- Jump
+								state 		<= s1;
+								--
+								PCSrc 		<= "10";
+								ALUOp 		<= "00";
+								PCWrite 	<= '1';
+								--
+								ALUSrcA 	<= '1';
+								ALUSrcB 	<= "00";
+								IRWrite 	<= '0';
+								MemWrite 	<= '0';
+								MemRead 	<= '0';
+								RegWrite 	<= '0';
+								RegDst 		<= 'X';
+								IorD 			<= 'X';
+								MemtoReg 	<= 'X';
+								Branch 		<= 'X';
+
+						when s7 => -- MemRead
+								state 		<= s11;
+								--
+								IorD 			<= '1';
+								MemRead 	<= '1';
+								--
+								ALUSrcA 	<= '1';
+								ALUSrcB 	<= "00";
+								ALUOp 		<= "00";
+								IRWrite 	<= '0';
+								MemWrite 	<= '0';
+								RegWrite 	<= '0';
+								PCWrite 	<= '0';
+								RegDst 		<= 'X';
+								MemtoReg 	<= 'X';
+								Branch 		<= 'X';
+								PCSrc 		<= "00";
+
+						when s8 => -- MemWrite
+								state 		<= s1;
+								--
+								IorD 			<= '1';
+								MemWrite 	<= '1';
+								--
+								ALUSrcA 	<= '1';
+								ALUSrcB 	<= "00";
+								ALUOp 		<= "00";
+								PCSrc 		<= "00";
+								IRWrite 	<= '0';
+								MemRead 	<= '0';
+								RegWrite 	<= '0';
+								PCWrite 	<= '0';
+								RegDst 		<= 'X';
+								MemtoReg 	<= 'X';
+								Branch 		<= 'X';
+
+						when s9 => -- ADDI Writeback (nuevo estado)
+								state 		<= s1;
+								--
+								RegDst 		<= '0';
+								MemtoReg 	<= '0';
+								RegWrite 	<= '1';
+								--
+								ALUSrcA 	<= '1';
+								ALUSrcB 	<= "00";
+								ALUOp 		<= "00";
+								PCSrc 		<= "00";
+								IRWrite 	<= '0';
+								MemRead 	<= '0';
+								PCWrite 	<= '0';
+								IorD 			<= '0';
+								MemWrite 	<= '0';
+								Branch 		<= 'X';
+
+						when s10 => -- ALU Writeback
+								state 		<= s1;
+								--
+								RegDst 		<= '1';
+								MemtoReg 	<= '0';
+								RegWrite 	<= '1';
+								--
+								ALUSrcA 	<= '1';
+								ALUSrcB 	<= "00";
+								ALUOp 		<= "00";
+								PCSrc 		<= "00";
+								IRWrite 	<= '0';
+								MemRead 	<= '0';
+								PCWrite 	<= '0';
+								IorD 			<= '0';
+								MemWrite 	<= '0';
+								Branch 		<= 'X';
+
+						when others => -- Mem Writeback (s11)
+								state 		<= s1;
+								--
+								RegDst 		<= '0';
+								MemtoReg 	<= '1';
+								RegWrite 	<= '1';
+								--
+								ALUSrcA 	<= '1';
+								ALUSrcB 	<= "00";
+								ALUOp 		<= "00";
+								PCSrc 		<= "00";
+								IRWrite 	<= '0';
+								MemRead 	<= '0';
+								PCWrite 	<= '0';
+								IorD 			<= '0';
+								MemWrite 	<= '0';
+								Branch 		<= 'X';
+
 					end case;
-					IRWrite <= '0';
-					PCWrite <= '0';
-					MemWrite <= '0';
-					MemRead <= '0';
-					RegWrite <= '0';
-					Branch <= '0';
-					RegDst <= 'X';
-					IorD <= 'X';
-					PCSrc <= "00";
-					MemtoReg <= 'X';
-				elsif (state = "0010") then
-					ALUSrcA <= '1';
-					ALUSrcB <= "10";
-					ALUOp <= "00";
-					case opcode is
-						when "1010"  => next_state <= "000011";
-						when others => next_state <= "000101";
-					end case;
-					--
-					IRWrite <= '0';
-					PCWrite <= '0';
-					MemWrite <= '0';
-					MemRead <= '0';
-					RegWrite <= '0';
-					Branch <= '0';
-					RegDst <= 'X';
-					IorD <= 'X';
-					PCSrc <= "00";
-					MemtoReg <= 'X';
-				elsif (state = "000110") then
-					ALUSrcA <= '1';
-					ALUSrcB <= "00";
-					ALUOp <= "10"; 
-					next_state <= "000111";
-					--
-					IRWrite <= '0';
-					PCWrite <= '0';
-					MemWrite <= '0';
-					MemRead <= '0';
-					RegWrite <= '0';
-					Branch <= '0';
-					RegDst <= 'X';
-					IorD <= 'X';
-					PCSrc <= "00";
-					MemtoReg <= 'X';
-				elsif (state = "001000") then
-					ALUSrcA <= '1';
-					ALUSrcB <= "00";
-					ALUOp <= "01";
-					PCSrc <= "01";
-					Branch <= '1';
-					next_state <= "000000";
-					--
-					IRWrite <= '0';
-					PCWrite <= '0';
-					MemWrite <= '0';
-					MemRead <= '0';
-					RegWrite <= '0';
-					RegDst <= 'X';
-					IorD <= 'X';
-					MemtoReg <= 'X';
-				elsif (state = "001001") then
-					PCSrc <= "01";
-					ALUOp <= "01";
-					PCWrite <= '1';
-					next_state <= "000000";
-					--
-					ALUSrcA <= '1';
-					ALUSrcB <= "00";
-					IRWrite <= '0';
-					MemWrite <= '0';
-					MemRead <= '0';
-					RegWrite <= '0';
-					RegDst <= 'X';
-					IorD <= 'X';
-					MemtoReg <= 'X';
-					Branch <= 'X';
-				elsif (state = "000011") then
-					IorD <= '1';
-					MemRead <= '1';
-					next_state <= "000100";
-					--
-					ALUSrcA <= '1';
-					ALUSrcB <= "00";
-					ALUOp <= "00";
-					IRWrite <= '0';
-					MemWrite <= '0';
-					RegWrite <= '0';
-					PCWrite <= '0';
-					RegDst <= 'X';
-					MemtoReg <= 'X';
-					Branch <= 'X';
-					PCSrc <= "00";
-				elsif (state = "000101") then
-					IorD <= '1';
-					MemWrite <= '1';
-					next_state <= "000000";
-					--
-					ALUSrcA <= '1';
-					ALUSrcB <= "00";
-					ALUOp <= "00";
-					IRWrite <= '0';
-					MemRead <= '0';
-					RegWrite <= '0';
-					PCWrite <= '0';
-					RegDst <= 'X';
-					MemtoReg <= 'X';
-					Branch <= 'X';
-					PCSrc <= "00";
-				else
-					RegDst <= '1';
-					MemtoReg <= '0';
-					RegWrite <= '1';
-					next_state <= "000000";
-					--
-					ALUSrcA <= '1';
-					ALUSrcB <= "00";
-					ALUOp <= "00";
-					IRWrite <= '0';
-					MemRead <= '0';
-					PCWrite <= '0';
-					IorD <= '0';
-					MemWrite <= '0';
-					Branch <= 'X';
-					PCSrc <= "00";
 				end if;
 		end process;
 end behavior;
